c75bb865a6cc859a0a027de9f8d9cd6194980ee1
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-10-06 17:58:22 -0600

    Se configura cors para realizar peticiones desde el front

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import 'dotenv/config'
 import router from './router';
 import { connectDb } from './config/db';
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 
 connectDb()
 
 const app = express()
+
+//cors
+app.use(cors(corsConfig))
 
 // Leer datos de formularios
 app.use(express.json())
